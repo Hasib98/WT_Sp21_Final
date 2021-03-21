182fe5c008b4efef84c53739bd99994f026727df
Hasib98 <phoenixoffire1998@gmail.com>
2021-03-21 15:51:36 +0600

    login cookie Complete

--- LAB1/Dashboard.php
+++ LAB1/Dashboard.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php
+$cookie_name = "hasib";
+$cookie_value = "hasib";
+setcookie($cookie_name, $cookie_value, time() + (30), "/"); // 86400 = 1 day
+?>
+<html>
 <body>
-    
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+    echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+    echo "Cookie '" . $cookie_name . "' is set!<br>";
+    echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+
 </body>
 </html>
