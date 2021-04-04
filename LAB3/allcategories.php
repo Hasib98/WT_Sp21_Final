cf028c3dda4b39664cdaa4147df345bd226ce930
Hasib98 <phoenixoffire1998@gmail.com>
2021-04-04 16:32:03 +0600

    LAB3  pre commit

---
+++ LAB3/allcategories.php
@@ -0,0 +1,25 @@
+<?php include 'admin_header.php';?>
+<!--All Categories starts -->
+
+<div class="center">
+	<h3 class="text">All Categories</h3>
+	<table class="table table-striped">
+		<thead>
+			<th>Sl#</th>
+			<th> Name</th>
+			<th>Product Count </th>
+			<th></th>
+			<th></th>
+			
+		</thead>
+		<tbody>
+			<td>1</td>
+			<td>Groserry</td>
+			<td>100</td>
+			<td><a href="editcategory.php" class="btn btn-success">Edit</a></td>
+			<td><a class="btn btn-danger">Delete</td>
+		</tbody>
+	</table>
+</div>
+<!--All Categories ends -->
+<?php include 'admin_footer.php';?>
